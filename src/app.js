c92939f3c798fd6c62d314995a652415194ca853
Michael Oyedele <oyedelemichael1@gmail.com>
2020-05-17 16:37:27 -0400

    removed port 3000

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ console.log(path.join(__dirname,'../public'))
 
 const app=express()
 
-const port=process.env.PORT ||3000
+const port=process.env.PORT 
 
 
 const publicdirectory=path.join(__dirname,'../public')
